e67dce7b80bfe46199aadb67aed650e55689f6d1
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-05-22 15:46:54 -0400

    Added Parallel_Variable_Assignment.rb

---
+++ Parallel_Variable_Assignment.rb
@@ -0,0 +1,14 @@
+#Parallel Variable Assignment
+
+# a = 10
+# b = 20
+# c = 30
+
+a, b, c = 10, 20, 30 #same as above
+
+p a, b, c #you can print out multiple variables with one line
+#puts a, b, c
+
+#Swaps the variables
+
+a, b = b, a
