b82dc1adaa4cb0f29e168088d45cf32bf61f63e1
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-05-22 15:32:59 -0400

    Added Variables.rb

---
+++ Variables.rb
@@ -0,0 +1,26 @@
+name = "Leo"
+last_name = "Francoeur"
+handsome = true
+age = 33 + 1
+
+puts name
+puts last_name
+puts name + " " + last_name
+
+puts handsome
+
+puts age + 5
+
+puts age
+
+age = 50
+
+puts age
+
+favorite_number = 24
+
+p favorite_number
+
+favorite_number = "Twenty-Four"
+
+p favorite_number
