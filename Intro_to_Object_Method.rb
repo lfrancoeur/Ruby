9ae614e15357bc442fed0ff647d6aeeb585f48fc
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-05-23 10:57:46 -0400

    Added Intro_to_Object_Method.rb

---
+++ Intro_to_Object_Method.rb
@@ -0,0 +1,18 @@
+p "hello world".length
+
+p "hello world".upcase
+
+p "hello world".downcase
+
+foo = "hello world"
+
+p foo.length
+p foo.upcase
+
+p 10.next #adds 1
+p -1.next
+
+puts
+
+puts "Hello World"
+puts "Hello World".inspect #gives a more literal view of an object
