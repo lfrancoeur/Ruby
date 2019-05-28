9f1437a64d22bf7295a85cbdef508a99a2bf3699
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-05-28 15:26:33 -0400

    Added Comparison_with_the_inequality_operator.rb

---
+++ Comparison_with_the_inequality_operator.rb
@@ -0,0 +1,10 @@
+p 10 != 5
+p 10 != 10
+p "Hello" != "Goodbye"
+p "Hello" != "Hello"
+p "Hello" != "hello" #case senetive! this will return true
+
+#for comaprison sake you can make a standard for data return
+p "Hello".downcase != "heLLo".downcase
+
+p "123" != 123
