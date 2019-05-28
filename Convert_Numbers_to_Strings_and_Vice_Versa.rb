7fd47512cc0d8e9609e2d7191bd97f151509c737
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-05-28 11:59:41 -0400

    Added Convert_Numbers_to_Strings_and_Vice_Versa.rb

---
+++ Convert_Numbers_to_Strings_and_Vice_Versa.rb
@@ -0,0 +1,23 @@
+str = "5"
+p str
+p str.class
+
+p str.to_i.class
+
+p str.to_f.class
+
+number = 10
+
+p number.class
+p number.to_s
+p number.to_f
+p number.to_f.class
+
+pi = 3.14
+
+p pi.class
+p pi.to_i
+p pi.to_i.class
+
+p pi.to_s
+p pi.class
