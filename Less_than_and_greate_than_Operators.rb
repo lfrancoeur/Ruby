37ccbc089c96da4f96d78a16cd5c75337021e863
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-05-28 15:37:44 -0400

    Less_than_and_greate_than_Operators.rb

---
+++ Less_than_and_greate_than_Operators.rb
@@ -0,0 +1,21 @@
+#less than
+p 1 < 8
+p 10 < 15
+p 12 < 5
+p 5 < 5
+
+#less than equal
+
+p 1 <= 5
+p 13 <= 5
+p 5 <= 5
+
+#greater than
+p 10 > 5
+p -1 > -5
+p 12 > 20
+
+#Greater than equal
+p 8 >= 5
+p 8 >= 10
+p 8 >= 8
