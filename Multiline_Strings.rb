da57d9853dc948acd9606b74ba4b5def63f701f2
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-06-03 12:09:35 -0400

    Added Multiline_strings.rb

---
+++ Multiline_Strings.rb
@@ -0,0 +1,12 @@
+#example of a multi line string below
+words = <<MLS
+  This will be a multi-line string
+  when it is output.
+
+The tabs will also be preserved.
+Just you watch.
+Goodbye!
+
+MLS
+
+puts words
