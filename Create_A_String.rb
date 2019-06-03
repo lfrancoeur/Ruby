09f8428da353a5c34be42af7e333dd43adda725f
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-06-03 11:54:34 -0400

    added Create_A_String.rb & The_Step_Method.rb

---
+++ Create_A_String.rb
@@ -0,0 +1,5 @@
+empty = ""
+p empty.length
+
+name = String.new("Boris")
+p name 
---
+++ The_step_Method.rb
@@ -0,0 +1,1 @@
+0.step(100, 5) { |number| puts number} #takes 2 arguments, the number you want to go to(end number) and the second value is how many steps you want to go up
