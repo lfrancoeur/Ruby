0be8c94f44622a363ad834c8c85aa78e26cc56c7
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-06-03 16:11:21 -0400

    added Escape_Characters.rb

---
+++ Escape_Characters.rb
@@ -0,0 +1,5 @@
+puts "Juliet said \"Goodbye\" to Romeo" #escaping the "" characters to show up as ""
+
+puts "Let's add a line break\nright here" #line break (\n)
+
+puts "\tInserts a tab" #\t is a tab
