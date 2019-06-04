4ce81d65b1458cc02566c685da5b46d94f26387a
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-06-04 11:33:09 -0400

    The_length_and_size_Methods.rb

---
+++ The_length_and_size_Methods.rb
@@ -0,0 +1,21 @@
+a = "Hello World"
+b = "Hi, my name is Boris!"
+c = " "
+d = ""
+
+p a.length
+p b.length
+p c.length
+p d.length
+
+puts
+
+p a.size
+p b.size
+p c.size
+p d.size
+
+#.length and .size are the same!
+
+p a.length.class
+p a.size.class 
