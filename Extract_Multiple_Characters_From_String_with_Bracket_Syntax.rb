bae3898236224719e46042a132e8f3b35afe2b36
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-06-04 13:47:29 -0400

    Added Extract_Multiple_Characters_From_String_with_Bracket_Syntax.rb

---
+++ Extract_Multiple_Characters_From_String_with_Bracket_Syntax.rb
@@ -0,0 +1,8 @@
+story = "Once upon a time in a land far, far away"
+
+p story[5, 4] #start at index position 5 and print 4 charaters
+p story.slice(5, 4) #same as above
+
+p story[0,5] #starts from 0 and shows 5 characters
+
+p story[0, story.length] #pulls the whole story variable 
