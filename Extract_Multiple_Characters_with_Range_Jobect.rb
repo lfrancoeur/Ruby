078a21042617d406fe86c34e6189356ddec9d321
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-06-04 15:48:44 -0400

    Added Extract_Multiple_Characters_with_Range_Jobect.rb

---
+++ Extract_Multiple_Characters_with_Range_Jobect.rb
@@ -0,0 +1,7 @@
+story = "Once upon a time in a land far, far away"
+
+p story[27..39] # inclusive of 27-39 (including 39)
+
+p story[27...39] # not inclusive of 27-39 (not including 39)
+
+p story[32..100] #goes to the end then just doesn't go any further
