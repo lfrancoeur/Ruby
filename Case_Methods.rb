fc1454264fcbda7b7afa3674a65de2e1e3100606
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-06-05 11:48:29 -0400

    Added Case_Methods.rb

---
+++ Case_Methods.rb
@@ -0,0 +1,11 @@
+puts "hello world".capitalize #converts first letter to capitals
+
+puts "heLLo world".capitalize #converts the LL to lower case and h to H
+
+puts "Hello world".capitalize
+
+puts "boris123".upcase #capitalizes everything
+
+puts "BORIS!@*&()".downcase #lower cases everything
+
+p "boRIs".swapcase #swaps cases of letter
