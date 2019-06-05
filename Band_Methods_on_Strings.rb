a74e5203c693e1d3156dffb64e14e9b37d9c1d92
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-06-05 15:15:34 -0400

    Added Band_Methods_on_Strings.rb

---
+++ Band_Methods_on_Strings.rb
@@ -0,0 +1,17 @@
+word = "hello"
+p word
+
+p word.capitalize! #sets variable word to the new capitalized one indefintiely(mutate)
+p word
+
+word.upcase!
+p word
+
+word.downcase!
+p word
+
+word.reverse!
+p word
+
+word.swapcase!
+p word
