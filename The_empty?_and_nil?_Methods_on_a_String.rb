1d9ac1cead0d28c1be5c6bfe1d1989ad57df4c62
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-06-05 15:38:07 -0400

    The_empty?_and_nil?_Methods_on_a_String.rb

---
+++ The_empty?_and_nil?_Methods_on_a_String.rb
@@ -0,0 +1,9 @@
+
+p "".empty? #checks if a strings length is empty
+
+p "".nil? #checks for nil
+
+name = "Donald Duck"
+
+last_name = name[100, 4] #start at character 100 in last name variable and print the last 4 which returns nil
+p last_name.nil? #tests if nil 
