a20ee4d7deec8c5dce9bf50982a82459488b67bf
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-06-06 14:49:41 -0400

    Added Return_Values_2.rb

---
+++ Return_Values_2.rb
@@ -0,0 +1,15 @@
+def nothing
+end
+
+p nothing #returns nil
+
+def return_string
+  "What will be the return value here?"
+end
+
+p return_string
+
+def return_guess
+  puts "What will be the return value here?" #this will return nil always
+end
+p return_guess
