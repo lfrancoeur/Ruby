0796acd98fe7a008b23ac51fdee9ac93b7c18129
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-06-06 13:38:19 -0400

    Added Local_Variables.rb

---
+++ Local_Variables.rb
@@ -0,0 +1,6 @@
+def introduce_myself #this is a method
+  expression = "I am a genius" #this is a local variable that only executes while the method executes
+  puts expression
+end
+
+introduce_myself
