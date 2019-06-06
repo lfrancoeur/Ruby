5062b03b3be0bbe661e09933614a50c24db1d05a
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-06-06 14:02:22 -0400

    Added Method_Parameters_and_Arguments.rb

---
+++ Method_Parameters_and_Arguments.rb
@@ -0,0 +1,10 @@
+def praise_person(name, age) #name is the parameter multiples uses a comma
+
+    puts "#{name} is amazing" #using the paremeter here
+    puts "#{name} is charming"
+    puts "#{name} is talented"
+    puts "His age in 5 years will be #{age + 5}"
+
+end
+
+praise_person("Leo", 34)
