bbda349a756c261f152c37319b1069095dce9b3c
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-06-07 12:59:03 -0400

    Added Thruthiness_and_Falsiness.rb

---
+++ Thruthiness_and_Falsiness.rb
@@ -0,0 +1,19 @@
+if 5 < 8
+  puts "This is true"
+end
+
+if 5 #ints are a truthy value
+  puts "That is true"
+end
+
+if "Hello" #all strings are true
+  puts "That is true"
+end
+
+if nil #falsie value
+  puts "That is true"
+end
+
+if false #falsie value
+  puts "That is true"
+end
