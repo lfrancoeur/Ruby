0a0d03ab37fdfd893d793d698ea261fa1099f04d
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-06-07 13:07:46 -0400

    Added If_elseif.rb

---
+++ If_elseif.rb
@@ -0,0 +1,19 @@
+color = "Yellow"
+
+if color == "Red"
+  puts "Red is rad"
+elsif color == "Green"
+  puts "Green is great!"
+elsif color == "Yellow"
+  puts "Yay for yellow!"
+end
+
+number = 55
+
+if number < 25
+  puts "That's a low number!"
+elsif number < 55
+  puts "That's a number in the middle!"
+elsif number < 75
+  puts "that's a big number"
+end
