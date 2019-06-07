c8a0b86148be2bc891e7c08beae537764f50be96
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-06-07 12:28:34 -0400

    The_if_Statment.rb

---
+++ The_if_Statment.rb
@@ -0,0 +1,27 @@
+if 5 < 7
+
+  p "That math statment is true!"
+
+end
+
+password = "topsecret"
+
+if password == "topsecret"
+  puts "Congrats, you've logged into our system!"
+end
+
+word = "kangaroo"
+
+if word.length == 8
+  puts "That word has 8 letters"
+end
+
+if word.include?("eb")
+
+  p "Yup, your word has eb in it"
+
+end
+
+if 5.odd?
+  p "That number is odd!"
+end
