3ce7775f9d953cc2e1964de18c9f3cc9e0bebae3
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-06-18 13:11:02 -0400

    Fixed up Fizzbuzz.rb again

--- Fizzbuzz.rb
+++ Fizzbuzz.rb
@@ -2,9 +2,9 @@ def fizzbuzz(number)
   #If the number is divisible by 3, output Fizz
   #If the number is divisible by5, output Buzz
   #If the number is divisible by BOTH 3 and 5, output Fizzbuzz
-  i = 0
+  i = 1
 
-  while i <= number 
+  while i <= number
   if i % 3 == 0 && !(i % 5 == 0)
     puts "#{i} = Fizz"
   elsif i % 5 == 0 && !(i % 3 == 0)
