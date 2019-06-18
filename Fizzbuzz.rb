009470e8a335f06ff35a41141fa6d18d2138ab29
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-06-18 13:10:11 -0400

    Fixed up Fizzbuzz.rb

--- Fizzbuzz.rb
+++ Fizzbuzz.rb
@@ -4,15 +4,13 @@ def fizzbuzz(number)
   #If the number is divisible by BOTH 3 and 5, output Fizzbuzz
   i = 0
 
-  while i <= number #until we get to the point that i is greater than 9 the loop will continue with waht's in the body
+  while i <= number 
   if i % 3 == 0 && !(i % 5 == 0)
     puts "#{i} = Fizz"
   elsif i % 5 == 0 && !(i % 3 == 0)
     puts "#{i} = Buzz"
-  elsif i % 15 == 0
+  elsif i % 3 == 0 && i % 5 == 0
     puts "#{i} = Fizzbuzz"
-  else
-    puts i
   end
   i += 1
   end
