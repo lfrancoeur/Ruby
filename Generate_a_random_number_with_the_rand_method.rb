488cabb9fe2759a0b9be283302e407c00fd9f19a
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-06-19 16:01:48 -0400

    Added Generate_a_random_number_with_the_rand_method.rb

---
+++ Generate_a_random_number_with_the_rand_method.rb
@@ -0,0 +1,9 @@
+puts rand #generates a random number between 0 and 1
+
+puts rand.round(2) #rounds to 2 digits
+
+puts rand(101) #generates a random number between 0 - 100
+
+puts rand * 100 #makes a floating point numbe between 0-100
+
+puts rand(1..100) #generates random numbers between 1 - 100
