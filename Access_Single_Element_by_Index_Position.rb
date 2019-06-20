2b06370c6767bc93008aa85475eeb5c3e32e132d
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-06-19 22:03:55 -0400

    Added Access_Single_Element_by_Index_Position.rb

---
+++ Access_Single_Element_by_Index_Position.rb
@@ -0,0 +1,11 @@
+
+fruits = ["Apple", "Orange", "Grape", "Banana"] #indexes start at 0
+
+p fruits.length #returns the total number of elements in an array
+
+p fruits[0] #pulls the first index which is Apple
+p fruits[10] #will be nil since index 10 doesn't exist
+
+p fruits[fruits.length - 1] # gets the last index position in the array
+p fruits.last # gets the last index position in the array
+p fruits[-1] # gets the last index position in the array
