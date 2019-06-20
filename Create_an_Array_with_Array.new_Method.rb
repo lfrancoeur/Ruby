efce173d1726425441a1e7c07fe29d44b222d162
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-06-19 21:13:36 -0400

    Added Create_an_Array_with_Array.new_Method.rb

---
+++ Create_an_Array_with_Array.new_Method.rb
@@ -0,0 +1,7 @@
+p Array.new #same as []
+
+p Array.new(3) #will create 3 objects with nil by default
+
+p Array.new(10, true) #second value is the value to be created e.g. 10 trues
+
+p Array.new(10, [1, 2, 3]) #creates 10 arrays with the values of 1, 2, 3
