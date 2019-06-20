8281305e83b53830642e60426c2047fd48704be9
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-06-19 20:48:16 -0400

    Added more examples to Introduction_to_Arrays.rb

--- Introduction_to_Arrays.rb
+++ Introduction_to_Arrays.rb
@@ -1,5 +1,27 @@
-p [] #this is a valid empty array
 
-p [].class
 
-numbers = []
+numbers = [4, 8, 15, 16, 23, 42]
+
+puts numbers
+p numbers
+print numbers
+
+puts #just a blank space to see the output better
+
+toys = ["Tedy bear", "Super Soaker", "board game"]
+p toys
+puts toys
+
+
+things = [4, true, "Hello", 10.99] #you can have multiple different data types in an array
+p things
+
+registrations = [true, true, false, true, false] #you can have duplicate values no problem
+p registrations
+
+students = [ ["Boris", 25, true], #putting arrays inside of arrays (3 arrays)
+            ["Sally", 23, true],
+            ["Ingrid", 31, false]]
+
+puts students
+p students
