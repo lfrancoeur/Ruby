e7beb3b8bd2a5f41b4b0e8857fb6e1fa8260eb79
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-06-26 16:13:06 -0400

    Added The_pop_Method.rb

---
+++ The_pop_Method.rb
@@ -0,0 +1,17 @@
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+p arr
+p arr.length
+
+arr.pop #removes the last element from the array
+
+p arr
+p arr.length
+
+last_item = arr.pop #you can store the last item
+p arr
+p last_item
+
+two_items = arr.pop(2) #removes the last two items
+p arr
+p two_items
