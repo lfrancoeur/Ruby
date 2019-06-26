97de6da870273760874c2b58353eaa2bf7e5aee9
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-06-26 10:47:13 -0400

    Added Access_Sequential_Array_Elements_with_Bracket_Syntax.rb

---
+++ Access_Sequential_Array_Elements_with_Bracket_Syntax.rb
@@ -0,0 +1,10 @@
+numbers = [1, 3, 5, 7, 9, 15, 21]
+
+p numbers[2, 4] #starts at index position 2 and pulls out 4 items (index positions 2,3,4,5)
+
+p numbers[1, 3]
+
+p numbers[0,1] #will get you a new array
+p numbers[0] #will not get you an array
+
+p numbers [4, 100] #will just show you the rest of the array since we don't have 100 values in the array
