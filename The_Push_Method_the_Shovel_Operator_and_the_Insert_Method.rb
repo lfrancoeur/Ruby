1d832b7dbf290c23322382333617112524430d2a
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-06-26 16:05:58 -0400

    Added The_Push_Method_the_Shovel_Operator_and_the_Insert_Method.rb

---
+++ The_Push_Method_the_Shovel_Operator_and_the_Insert_Method.rb
@@ -0,0 +1,11 @@
+locations = ["House", "Airport", "Bar"]
+p locations
+
+locations.push("Restaurant", "Saloon") #adds to the end of the array
+p locations
+
+locations << "Wendys" << "McDonalds" #adds to the end of the array
+p locations
+
+locations.insert(1, "Out House", "Cafe") #takes a minimum of 2 arguments, index position and what you want to add in the second
+p locations
