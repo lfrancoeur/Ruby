a9c32fdb83f2ad1f2374e329df833958632da465
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-06-26 11:25:34 -0400

    Added The_Slice_Method.rb

---
+++ The_slice_Method.rb
@@ -0,0 +1,7 @@
+numbers = [0, 2, 4, 8, 10, 12, 14]
+
+p numbers.slice(3) #shows the 3rd index position
+p numbers.slice(100) #shows a nill
+p numbers.slice(2, 3) #Starts at 2 and adds 3 indexes (shows index 2,3,4)
+p numbers.slice(4..6) #starts at 4 and ends at index 6
+p numbers.slice(4...6) #starts at 4 and ends at 5 
