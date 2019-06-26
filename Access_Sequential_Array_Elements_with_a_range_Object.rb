687009bab114909fa2ec5df3966bf36df5b0085a
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-06-26 10:59:30 -0400

    Add Access_Sequential_Array_Elements_with_a_range_Object.rb

---
+++ Access_Sequential_Array_Elements_with_a_range_Object.rb
@@ -0,0 +1,6 @@
+numbers = [1, 3, 5, 7, 9, 15, 21, 18, 6]
+
+p numbers[3..6] #start at index 3 and end at 6 (inclisive of 3-6)
+p numbers[3..3] #outputs index 3
+
+p numbers[4...8] #start at index 4 and end at 7 (excludes 8)  
