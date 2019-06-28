92bf4dd0973ac0dc05401915ef0fcac6e1d3b674
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-06-28 11:18:07 -0400

    Added Convert_Range_To_Array.rb

---
+++ Convert_Range_To_Array.rb
@@ -0,0 +1,12 @@
+letters_range = "A".."T"
+p letters_range
+letters_array = letters_range.to_a #converts it to an array
+
+p letters_array[-1]
+
+
+numbers_range = 415..450
+p numbers_range
+number_array = numbers_range.to_a
+p number_array.size
+p number_array[1]
