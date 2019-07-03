f701481975897f3b3283c877855adc357e8b5891
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-07-03 11:24:50 -0400

    Initial commit of The_reverse_Method_on_an_Array.rb

---
+++ The_reverse_Method_on_an_Array.rb
@@ -0,0 +1,9 @@
+p [1, 2, 3].reverse
+p ["A", "B", "C"].reverse
+p [true, true, false, false, true].reverse
+
+queue = [4, 8, 15, 16, 23, 42]
+p queue
+
+queue.reverse! #mutates the array to the reverse
+p queue
