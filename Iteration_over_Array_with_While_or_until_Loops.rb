aa6ff2d1a1fd36adb19744571b956a272da67ead
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-07-03 10:40:34 -0400

    Initial commit of Iteration_over_Array_with_While_or_until_Loops.rb

---
+++ Iteration_over_Array_with_While_or_until_Loops.rb
@@ -0,0 +1,9 @@
+animals = ["Zebra", "Baboon", "Cheetah", "Elaphant", "Kangaroo"]
+
+i = 0
+
+while i < animals.length #works with until (until i == animals.length)
+  puts i
+  puts animals[i]
+  i += 1
+end
