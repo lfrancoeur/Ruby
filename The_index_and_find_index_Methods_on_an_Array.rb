df8a766d1506465427a8437e4bb8c79868eb0922
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-07-03 16:13:47 -0400

    Initial commit of The_index_and_find_index_Methods_on_an_Array.rb

---
+++ The_index_and_find_index_Methods_on_an_Array.rb
@@ -0,0 +1,6 @@
+colors = ["Red", "Blue", "Green", "Red"]
+
+p colors.index("Green")
+p colors.index("Red") #returns the first occurance of reds index
+
+p colors.find_index("Red") #the same as .index 
