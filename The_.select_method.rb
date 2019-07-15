2a3d20977e7c96b709ec6510c7d516f4f0a12b18
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-07-15 10:41:28 -0400

    Initial commit of The_.select_method.rb

---
+++ The_.select_method.rb
@@ -0,0 +1,13 @@
+grades = [80, 95, 13, 76, 28, 39]
+
+matches = grades.select do |number|
+  number >= 75 #must evaluate to true/false
+end
+
+p matches
+
+words = ["level", "selfless", "racecar", "dinosaur"]
+
+palindromes = words.select { |word| word == word.reverse}
+
+p palindromes
