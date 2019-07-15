d19beb8d93957833a23a2e7bf85ffa7808df4252
Leo Francoeur <lfrancoeur@lfrancoeur-m1.fuze.global>
2019-07-15 10:55:44 -0400

    Initial Commit of Unpack_A_Multidimensional_Array.rb

---
+++ Unpack_A_Multidimensional_Array.rb
@@ -0,0 +1,9 @@
+users = [["Bob", 25, "Male"], ["Susan", 48, "Female"], ["Larry", 62, "Male"]] # <-- multi dimensional array
+
+p users[1][2] #Takes the second array and extract element 2
+
+bob, susan, larry = users #unpacks the array and assiangs each multidimensional array and put it in the variables on the left
+
+p bob
+p susan
+p larry
